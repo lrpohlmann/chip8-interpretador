fd2807744f38722dfa5139d6865637b0c0d72196
Leonardo <leo.pohlmann@hotmail.com>
2022-03-07 19:18:39 -0300

    escrever contexto runtime atualizado

--- chip8/nucleo/operacoes/codigo_contexto_runtime.py
+++ chip8/nucleo/operacoes/codigo_contexto_runtime.py
@@ -1,5 +1,5 @@
 from typing import Union
-from chip8.nucleo.dados import criar_contexto_runtime, e_ram
+from chip8.nucleo.dados import e_ram
 from chip8.nucleo.dados.tipos import CONTADOR, CONTEXTO_RUNTIME, CONTEXTO_RUNTIME_KEYS, INSTRUCAO_COMPLETA_CHIP8, PIXEL_MAP, RAM, REGISTRADOR_INDEX, REGISTRADORES, e_contador, e_instrucao, e_pixel_map, e_registrador, e_registrador_index
 
 
@@ -12,19 +12,23 @@ def ler_contexto_runtime(contexto: CONTEXTO_RUNTIME, chave: CONTEXTO_RUNTIME_KEY
 
 
 def escrever_contexto_runtime(contexto: CONTEXTO_RUNTIME, chave: CONTEXTO_RUNTIME_KEYS, valor: Union[RAM, REGISTRADORES, REGISTRADOR_INDEX, CONTADOR, PIXEL_MAP, INSTRUCAO_COMPLETA_CHIP8]) -> CONTEXTO_RUNTIME:
-    if chave == "ram" and e_ram(valor):
-        return contexto.set(chave, valor)
-    elif chave == "registradores" and e_registrador(valor):
-        return contexto.set(chave, valor)
-    elif chave == "registrador_index" and e_registrador_index(valor):
-        return contexto.set(chave, valor)
-    elif chave == "contador" and e_contador(valor):
-        return contexto.set(chave, valor)
-    elif chave == "pixel_map" and e_pixel_map(valor):
-        return contexto.set(chave, valor)
-    elif chave == "ultima_instrucao" and e_instrucao(valor):
-        return contexto.set(chave, valor)
-    elif chave == "ultima_execucao":
-        return contexto.set(chave, valor)
-    else:
-        raise Exception()
+    key_typeguard = {
+        "ram": e_ram,
+        "registradores": e_registrador,
+        "registrador_index": e_registrador_index,
+        "contador": e_contador,
+        "pixel_map": e_pixel_map,
+        "ultima_instrucao": e_instrucao,
+        "ultima_execucao": lambda x: True
+    }
+
+    try:
+        t_guard = key_typeguard[chave]
+        if t_guard(valor):
+            return contexto.set(chave, valor)
+
+        else:
+            raise Exception(
+                f"CONTEXTO RUNTIME: valor '{valor}' não é válido para a chave '{chave}'")
+    except KeyError:
+        raise Exception(f"CONTEXTO RUNTIME: chave '{chave}' inexistente.")
