b58fc7f3f35ec03045709e80c49a8b6b0795dc81
leonardo <leo.pohlmann@hotmail.com>
2023-04-27 17:49:31 -0300

    retirada immutables

--- chip8/servicos/log/formatador.py
+++ chip8/servicos/log/formatador.py
@@ -2,8 +2,6 @@ from typing import Any, Callable, Dict, Literal, Sequence, Union
 from collections.abc import Mapping
 from functools import partial, singledispatch
 
-from immutables import Map
-
 
 def _formatador_base(a_ser_formatado, formatador_especifico: Callable[[Any], str]):
     return formatador_especifico(a_ser_formatado)
@@ -34,5 +32,4 @@ def _dict_str_formatador_mostrar_apenas_valores_nao_nulos(obj: Mapping):
     return str(dicio_para_transformar_em_str)
 
 
-formatador_a = partial(
-    _formatador_base, formatador_especifico=_formatador_especifico)
+formatador_a = partial(_formatador_base, formatador_especifico=_formatador_especifico)
