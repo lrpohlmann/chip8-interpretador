e49d379160ac35498497c7d2ebead436dd7e14e3
leonardo <leo.pohlmann@hotmail.com>
2023-04-27 17:39:35 -0300

    renomeado pasta

--- chip8/nucleo/dados/tipos.py
+++ chip8/nucleo/dados/tipos.py
@@ -1,102 +1,121 @@
 from ast import Call
 from collections import namedtuple
-from typing import Any, Callable, Dict, List, Literal, Mapping, MutableMapping, Optional, Sequence, Tuple, TypeVar, Union, NewType
-from typing_extensions import TypeGuard
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Literal,
+    Mapping,
+    MutableMapping,
+    Optional,
+    Sequence,
+    Tuple,
+    TypeVar,
+    Union,
+    NewType,
+    TypeGuard,
+)
 
 from pyrsistent.typing import PVector
 
 from chip8.servicos.hexadecimais.algarismo import SEQUENCIA_ALGARISMOS_HEXADECIMAIS
 from immutables import Map
 
-DIGITOS_HEXADECIMAIS = Literal["0", "1", "2", "3", "4",
-                               "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+DIGITOS_HEXADECIMAIS = Literal[
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"
+]
 RAM = NewType("RAM", Map[str, Optional[str]])
 REGISTRADORES = NewType("REGISTRADORES", Map[str, Optional[str]])
-REGISTRADOR_INDEX = NewType(
-    "REGISTRADOR_INDEX", Map[Literal["i"], Optional[str]])
+REGISTRADOR_INDEX = NewType("REGISTRADOR_INDEX", Map[Literal["i"], Optional[str]])
 CONTADOR = NewType("CONTADOR", str)
 CALL_STACK = NewType("CALL_STACK", PVector[str])
 INSTRUCAO_COMPLETA_CHIP8 = NewType("INSTRUCAO_COMPLETA_CHIP8", str)
 SEQUENCIA_INSTRUCOES_CHIP8_FRACIONADAS_EM_2BYTES = TypeVar(
-    "SEQUENCIA_INSTRUCOES_CHIP8_FRACIONADAS_EM_2BYTES", Sequence[str], List[str])
+    "SEQUENCIA_INSTRUCOES_CHIP8_FRACIONADAS_EM_2BYTES", Sequence[str], List[str]
+)
 SPRITE = Sequence[str]
 PIXEL_MAP_COORDENADA_X = NewType("PIXEL_MAP_COORDENADA_X", int)
 PIXEL_MAP_COORDENADA_Y = NewType("PIXEL_MAP_COORDENADA_Y", int)
 PIXEL_MAP = NewType(
-    "PIXEL_MAP", Map[Tuple[PIXEL_MAP_COORDENADA_X, PIXEL_MAP_COORDENADA_Y], Literal[0, 1]])
+    "PIXEL_MAP",
+    Map[Tuple[PIXEL_MAP_COORDENADA_X, PIXEL_MAP_COORDENADA_Y], Literal[0, 1]],
+)
 
 R = TypeVar("R")
 FUNCOES_EXECUCAO = Callable[[R], R]
 
-CONTEXTO_RUNTIME_KEYS = Literal["ram", "registradores",
-                                "registrador_index", "contador", "pixel_map", "ultima_instrucao", "ultima_execucao"]
-CONTEXTO_RUNTIME = Map[CONTEXTO_RUNTIME_KEYS,
-                       Union[RAM, REGISTRADORES, REGISTRADOR_INDEX, CONTADOR, PIXEL_MAP, INSTRUCAO_COMPLETA_CHIP8, FUNCOES_EXECUCAO]]
+CONTEXTO_RUNTIME_KEYS = Literal[
+    "ram",
+    "registradores",
+    "registrador_index",
+    "contador",
+    "pixel_map",
+    "ultima_instrucao",
+    "ultima_execucao",
+]
+CONTEXTO_RUNTIME = Map[
+    CONTEXTO_RUNTIME_KEYS,
+    Union[
+        RAM,
+        REGISTRADORES,
+        REGISTRADOR_INDEX,
+        CONTADOR,
+        PIXEL_MAP,
+        INSTRUCAO_COMPLETA_CHIP8,
+        FUNCOES_EXECUCAO,
+    ],
+]
 C = TypeVar("C")
 CONTEXTO_RUNTIME_FUNC_ATUALIZAR = Callable[[C], C]
 
 
 def e_instrucao(obj: Any) -> TypeGuard[INSTRUCAO_COMPLETA_CHIP8]:
-    return all([isinstance(obj, str), len(obj) == 4, all(
-        [(digito in SEQUENCIA_ALGARISMOS_HEXADECIMAIS) for digito in obj])])
+    return all(
+        [
+            isinstance(obj, str),
+            len(obj) == 4,
+            all([(digito in SEQUENCIA_ALGARISMOS_HEXADECIMAIS) for digito in obj]),
+        ]
+    )
 
 
 def e_ram(obj: Any) -> TypeGuard[RAM]:
-    valores = all(
-        [
-            (val == None or type(val) == str) for val in obj.values()
-        ]
-    )
-    keys = all(
-        [
-            (type(val) == str) for val in obj.keys()
-        ]
-    )
+    valores = all([(val == None or type(val) == str) for val in obj.values()])
+    keys = all([(type(val) == str) for val in obj.keys()])
 
     return all([keys, valores])
 
 
 def e_registrador(obj: Any) -> TypeGuard[REGISTRADORES]:
-    valores = all(
-        [
-            (val == None or isinstance(val, str)) for val in obj.values()
-        ]
-    )
+    valores = all([(val == None or isinstance(val, str)) for val in obj.values()])
 
-    keys = all(
-        [
-            val in SEQUENCIA_ALGARISMOS_HEXADECIMAIS for val in obj.keys()
-        ]
-    )
+    keys = all([val in SEQUENCIA_ALGARISMOS_HEXADECIMAIS for val in obj.keys()])
 
     return all([keys, valores])
 
 
 def e_registrador_index(obj: Any) -> TypeGuard[REGISTRADOR_INDEX]:
-    keys = (len(obj.keys()) == 1 and list(obj.keys())[0] == "i")
+    keys = len(obj.keys()) == 1 and list(obj.keys())[0] == "i"
 
-    valores = (isinstance(obj["i"], str) or obj["i"] == None)
+    valores = isinstance(obj["i"], str) or obj["i"] == None
 
     return all([keys, valores])
 
 
 def e_contador(obj: Any) -> TypeGuard[CONTADOR]:
-    return all([isinstance(obj, str), all(
-        [(digito in SEQUENCIA_ALGARISMOS_HEXADECIMAIS) for digito in obj])])
+    return all(
+        [
+            isinstance(obj, str),
+            all([(digito in SEQUENCIA_ALGARISMOS_HEXADECIMAIS) for digito in obj]),
+        ]
+    )
 
 
 def e_pixel_map(obj: Any) -> TypeGuard[PIXEL_MAP]:
-    valores = all(
-        [
-            isinstance(val, int) for val in obj.values()
-        ]
-    )
+    valores = all([isinstance(val, int) for val in obj.values()])
 
-    keys = all(
-        [
-            isinstance(val, tuple) for val in obj.keys()
-        ]
-    )
+    keys = all([isinstance(val, tuple) for val in obj.keys()])
 
     return all([keys, valores])
 
--- chip8/servicos/log/decorador.py
+++ chip8/servicos/log/decorador.py
@@ -1,9 +1,8 @@
 import pprint
 import logging
-from typing import Any, Callable, Dict, Iterable, Sequence, TypeVar
+from typing import Any, Callable, Dict, Iterable, Sequence, TypeVar, ParamSpec
 import functools
 import inspect
-from typing_extensions import ParamSpec
 
 from chip8.servicos.log.formatador import formatador_a
 
@@ -20,22 +19,30 @@ def log_parametros_e_retorno_da_funcao(func: Callable[P, R]):
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         resultado = func(*args, **kwargs)
 
-        logger.debug(_log_str_formato(func.__name__,
-                                      _formatar_str_log_parametros(
-                                          _associar_argumentos_com_parametros(
-                                              func, args, kwargs), formatador_a  # type: ignore
-                                      ),
-                                      _formatar_str_log_resultado(resultado, formatador_a)))
+        logger.debug(
+            _log_str_formato(
+                func.__name__,
+                _formatar_str_log_parametros(
+                    _associar_argumentos_com_parametros(func, args, kwargs),
+                    formatador_a,  # type: ignore
+                ),
+                _formatar_str_log_resultado(resultado, formatador_a),
+            )
+        )
         return resultado
 
     return wrapper
 
 
-def _log_str_formato(func_name, associacao_argumento_parametro_str: str, resultado_str: str):
+def _log_str_formato(
+    func_name, associacao_argumento_parametro_str: str, resultado_str: str
+):
     return f"Função: {func_name}, Parametros: {associacao_argumento_parametro_str}, Resultado: {resultado_str}"
 
 
-def _formatar_str_log_resultado(a_ser_formatado, formatador: Callable[[Any], str]) -> str:
+def _formatar_str_log_resultado(
+    a_ser_formatado, formatador: Callable[[Any], str]
+) -> str:
     str_inicial = "["
     str_final = "]"
     for x in a_ser_formatado:
@@ -44,7 +51,9 @@ def _formatar_str_log_resultado(a_ser_formatado, formatador: Callable[[Any], str
     return str_inicial + str_final
 
 
-def _formatar_str_log_parametros(a_ser_formatado: Dict, formatador: Callable[[Any], str]) -> str:
+def _formatar_str_log_parametros(
+    a_ser_formatado: Dict, formatador: Callable[[Any], str]
+) -> str:
     str_inicial = "{"
     str_final = "}"
     for k, v in a_ser_formatado.items():
@@ -54,33 +63,58 @@ def _formatar_str_log_parametros(a_ser_formatado: Dict, formatador: Callable[[An
     return associacao_formatada
 
 
-def _associar_argumentos_com_parametros(func: Callable, args: Sequence, kwargs: Dict) -> Dict:
+def _associar_argumentos_com_parametros(
+    func: Callable, args: Sequence, kwargs: Dict
+) -> Dict:
     nome_params_func = inspect.signature(func).parameters.keys()
     parametros_args_e_kwargs_fornecidos = _juntar_parametros(args, kwargs)
 
-    quantidade_parametros_faltantes = len(nome_params_func) - \
-        len(parametros_args_e_kwargs_fornecidos)
+    quantidade_parametros_faltantes = len(nome_params_func) - len(
+        parametros_args_e_kwargs_fornecidos
+    )
     parametros_padrao = list(inspect.signature(func).parameters.values())
     qtd_parametros_padrao = len(parametros_padrao)
 
     if quantidade_parametros_faltantes == qtd_parametros_padrao:
-        return dict(zip(nome_params_func, _juntar_parametros(args, kwargs, parametros_padrao)))
+        return dict(
+            zip(nome_params_func, _juntar_parametros(args, kwargs, parametros_padrao))
+        )
 
     elif quantidade_parametros_faltantes < qtd_parametros_padrao:
-        differenca_parametros_fornecidos_e_padrao = qtd_parametros_padrao - \
-            quantidade_parametros_faltantes
+        differenca_parametros_fornecidos_e_padrao = (
+            qtd_parametros_padrao - quantidade_parametros_faltantes
+        )
 
-        parametros_padrao_para_completar_parametros_faltantes = parametros_padrao[differenca_parametros_fornecidos_e_padrao: len(
-            parametros_padrao) + 1]
+        parametros_padrao_para_completar_parametros_faltantes = parametros_padrao[
+            differenca_parametros_fornecidos_e_padrao : len(parametros_padrao) + 1
+        ]
 
-        return dict(zip(nome_params_func, _juntar_parametros(args, kwargs, parametros_padrao_para_completar_parametros_faltantes)))
+        return dict(
+            zip(
+                nome_params_func,
+                _juntar_parametros(
+                    args, kwargs, parametros_padrao_para_completar_parametros_faltantes
+                ),
+            )
+        )
 
     elif quantidade_parametros_faltantes == 0:
-        return dict(zip(nome_params_func, _juntar_parametros(parametros_args_e_kwargs_fornecidos)))
+        return dict(
+            zip(
+                nome_params_func,
+                _juntar_parametros(parametros_args_e_kwargs_fornecidos),
+            )
+        )
 
     else:
         raise Exception()
 
 
-def _juntar_parametros(args: Sequence, kwargs: Dict = {}, padrao: Iterable[inspect.Parameter] = []) -> Sequence:
-    return list(args) + list(kwargs.values()) + [p.default for p in padrao if p.default != inspect.Parameter.empty]
+def _juntar_parametros(
+    args: Sequence, kwargs: Dict = {}, padrao: Iterable[inspect.Parameter] = []
+) -> Sequence:
+    return (
+        list(args)
+        + list(kwargs.values())
+        + [p.default for p in padrao if p.default != inspect.Parameter.empty]
+    )
